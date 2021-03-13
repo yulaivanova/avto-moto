73be47e2248f8ad628484b1f91e414506dbc930f
yuliaivanova <yulialepehina@gmail.com>
2021-03-13 17:16:25 +0300

    Исправления поппа и изображений

--- source/js/map.js
+++ source/js/map.js
@@ -1,29 +1,26 @@
-(function () {
-    ymaps.ready(function () {
-    var myMap = new ymaps.Map('map', {
-      center: [59.968137, 30.316263],
-      zoom: 16
+function init(ymaps) {
+  var myMap = new ymaps.Map('map', {
+    center: [59.968137, 30.316263],
+    zoom: 16
+  }, {
+    searchControlProvider: 'yandex#search'
+  }),
+
+    myPlacemark = new ymaps.Placemark([59.968137, 30.316263], {
+      balloonContent: 'Avto moto'
     }, {
-      searchControlProvider: 'yandex#search'
-    }),
+      // Опции.
+      // Необходимо указать данный тип макета.
+      iconLayout: 'default#image',
+      // Своё изображение иконки метки.
+      iconImageHref: 'img/map-pin.png',
+      // Размеры метки.
+      iconImageSize: [32, 40],
+      // Смещение левого верхнего угла иконки относительно
+      // её "ножки" (точки привязки).
+      iconImageOffset: [-20, -45]
+    });
 
-      myPlacemark = new ymaps.Placemark([59.968137, 30.316263], {
-        balloonContent: 'Avto moto'
-      }, {
-        // Опции.
-        // Необходимо указать данный тип макета.
-        iconLayout: 'default#image',
-        // Своё изображение иконки метки.
-        iconImageHref: 'img/map-pin.png',
-        // Размеры метки.
-        iconImageSize: [32, 40],
-        // Смещение левого верхнего угла иконки относительно
-        // её "ножки" (точки привязки).
-        iconImageOffset: [-20, -45]
-      });
-
-    myMap.geoObjects
-      .add(myPlacemark);
-  });
-
-})();
+  myMap.geoObjects
+    .add(myPlacemark);
+};
